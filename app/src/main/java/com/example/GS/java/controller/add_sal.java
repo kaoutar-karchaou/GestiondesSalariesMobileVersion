a67d110899c79fdffd8fe7e58d351563eb7f1e49
kaoutar karchaou <kawtarlulu.kl@gmail.com>
2020-05-29 17:11:06 +0100

    add sal , model salarie

---
+++ app/src/main/java/com/example/GS/java/controller/add_sal.java
@@ -0,0 +1,16 @@
+package com.example.GS.java.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.GS.R;
+
+public class add_sal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_sal);
+    }
+}
--- app/src/main/java/com/example/GS/java/model/Salaries.java
+++ app/src/main/java/com/example/GS/java/model/Salaries.java
@@ -1,5 +1,145 @@
 package com.example.GS.java.model;
 
-public class Salaries {
+import java.util.Date;
 
+public class Salaries {
+    private String id ;
+    private String nom  ;
+    private String prenom  ;
+    private String cin ;
+    private String adresse ;
+    private String telephone;
+    private String email;
+    private Date dateNaissance;
+    private String departement ;
+    private String emploiOccupe;
+    private int Anciennete ;
+    private double salaireBase ;
+
+    public Salaries() {
+    }
+
+    public Salaries(String nom, String prenom, String cin, String adresse, String telephone, String email, Date dateNaissance, String departement, String emploiOccupe, int anciennete, double salaireBase) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.cin = cin;
+        this.adresse = adresse;
+        this.telephone = telephone;
+        this.email = email;
+        this.dateNaissance = dateNaissance;
+        this.departement = departement;
+        this.emploiOccupe = emploiOccupe;
+        Anciennete = anciennete;
+        this.salaireBase = salaireBase;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getCin() {
+        return cin;
+    }
+
+    public void setCin(String cin) {
+        this.cin = cin;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public void setDateNaissance(Date dateNaissance) {
+        this.dateNaissance = dateNaissance;
+    }
+
+    public String getDepartement() {
+        return departement;
+    }
+
+    public void setDepartement(String departement) {
+        this.departement = departement;
+    }
+
+    public String getEmploiOccupe() {
+        return emploiOccupe;
+    }
+
+    public void setEmploiOccupe(String emploiOccupe) {
+        this.emploiOccupe = emploiOccupe;
+    }
+
+    public int getAnciennete() {
+        return Anciennete;
+    }
+
+    public void setAnciennete(int anciennete) {
+        Anciennete = anciennete;
+    }
+
+    public double getSalaireBase() {
+        return salaireBase;
+    }
+
+    public void setSalaireBase(double salaireBase) {
+        this.salaireBase = salaireBase;
+    }
+
+    @Override
+    public String toString() {
+        return "Salaries{" +
+                "id='" + id + '\'' +
+                ", nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", cin='" + cin + '\'' +
+                ", adresse='" + adresse + '\'' +
+                ", telephone='" + telephone + '\'' +
+                ", email='" + email + '\'' +
+                ", dateNaissance=" + dateNaissance +
+                ", departement='" + departement + '\'' +
+                ", emploiOccupe='" + emploiOccupe + '\'' +
+                ", Anciennete=" + Anciennete +
+                ", salaireBase=" + salaireBase +
+                '}';
+    }
 }
