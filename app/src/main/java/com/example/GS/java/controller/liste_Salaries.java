f2a9f860ad1c894de812055c3287fec1e1e6524a
kaoutar karchaou <kawtarlulu.kl@gmail.com>
2020-05-29 16:43:44 +0100

    creation des packages controller-model-database creation de liste des salaries model salaries

--- app/src/main/java/com/example/GS/java/controller/liste_Salaries.java
+++ app/src/main/java/com/example/GS/java/controller/liste_Salaries.java
@@ -1,14 +1,16 @@
-package com.example.GS;
+package com.example.GS.java.controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.GS.R;
+
+public class liste_Salaries extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_liste__salaries);
     }
 }
---
+++ app/src/main/java/com/example/GS/java/model/Salaries.java
@@ -0,0 +1,5 @@
+package com.example.GS.java.model;
+
+public class Salaries {
+
+}
